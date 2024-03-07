fbd4d6f185b0eb56e47858d6d00ba521c11b6381
Blane <161300198+Blane187@users.noreply.github.com>
2024-03-07 16:27:31 +0700

    Update extra.py

--- tabs/extra/extra.py
+++ tabs/extra/extra.py
@@ -2,6 +2,7 @@ import gradio as gr
 
 import tabs.extra.processing.processing as processing
 import tabs.extra.analyzer.analyzer as analyzer
+import tabs.extra.yt_dlp.yt_dlp as yt_dlp
 
 from assets.i18n.i18n import I18nAuto
 
@@ -17,6 +18,9 @@ def extra_tab():
 
     with gr.TabItem(i18n("Processing")):
         processing.processing()
+        
+    with gr.TabItem("Processing"):
+        yt_dlp.yt_dlp()
 
     with gr.TabItem(i18n("Audio Analyzer")):
         analyzer.analyzer()
